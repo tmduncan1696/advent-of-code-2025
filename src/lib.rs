00d6ee0c143f7b7df94f3064c2becc77d4710f4d
Thomas Duncan <tmduncan1696@gmail.com>
2025-12-29 08:39:35 -0500

    Update lib.rs for Day 05

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-const MAX_DAY: u8 = 4;
+const MAX_DAY: u8 = 5;
 
 pub mod args;
 use crate::args::Args;
@@ -7,7 +7,7 @@ pub mod day01;
 pub mod day02;
 pub mod day03;
 pub mod day04;
-// pub mod day05;
+pub mod day05;
 // pub mod day06;
 // pub mod day07;
 // pub mod day08;
@@ -15,19 +15,6 @@ pub mod day04;
 // pub mod day10;
 // pub mod day11;
 // pub mod day12;
-// pub mod day13;
-// pub mod day14;
-// pub mod day15;
-// pub mod day16;
-// pub mod day17;
-// pub mod day18;
-// pub mod day19;
-// pub mod day20;
-// pub mod day21;
-// pub mod day22;
-// pub mod day23;
-// pub mod day24;
-// pub mod day25;
 
 pub struct RunResult {
     pub day: u8,
@@ -67,7 +54,7 @@ fn run_day(day: u8) -> RunResult {
         2 => day02::run,
         3 => day03::run,
         4 => day04::run,
-        // 5 => day05::run,
+        5 => day05::run,
         // 6 => day06::run,
         // 7 => day07::run,
         // 8 => day08::run,
@@ -75,19 +62,6 @@ fn run_day(day: u8) -> RunResult {
         // 10 => day10::run,
         // 11 => day11::run,
         // 12 => day12::run,
-        // 13 => day13::run,
-        // 14 => day14::run,
-        // 15 => day15::run,
-        // 16 => day16::run,
-        // 17 => day17::run,
-        // 18 => day18::run,
-        // 19 => day19::run,
-        // 20 => day20::run,
-        // 21 => day21::run,
-        // 22 => day22::run,
-        // 23 => day23::run,
-        // 24 => day24::run,
-        // 25 => day25::run,
         _ => panic!("There is no day {day} in Advent of Code")
     };
 
